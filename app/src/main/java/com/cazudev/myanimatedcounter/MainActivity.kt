d3b1873f541aad24b5a0f2e518ad28487cf8ec97
Carlos <carlos.canete@espaciosoa.com>
2024-06-19 14:43:43 +0200

    add counter and button action to main screen

--- app/src/main/java/com/cazudev/myanimatedcounter/MainActivity.kt
+++ app/src/main/java/com/cazudev/myanimatedcounter/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.cazudev.myanimatedcounter.ui.theme.MyAnimatedCounterTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +24,20 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyAnimatedCounterTheme {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    var count by remember {
+                        mutableIntStateOf(0)
+                    }
+                    AnimatedCounter(count = count, style = MaterialTheme.typography.headlineMedium)
 
+                    Button(onClick = { count++ }) {
+                        Text(text = "Increment")
+                    }
+                }
             }
         }
     }
