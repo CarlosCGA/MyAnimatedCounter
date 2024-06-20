a616b4b01a58cee27cecb3fe0e820fbfac18d9d9
Carlos <carlos.canete@espaciosoa.com>
2024-06-20 17:37:40 +0200

    create smooth tick slider

---
+++ app/src/main/java/com/cazudev/myanimatedcounter/MySmoothTickSlider.kt
@@ -0,0 +1,61 @@
+package com.cazudev.myanimatedcounter
+
+import android.util.Log
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ThumbUp
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import kotlinx.coroutines.delay
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+fun MySmoothTickSlider() {
+    var progress by rememberSaveable {
+        mutableFloatStateOf(0F)
+    }
+    var progressTransition by rememberSaveable {
+        mutableFloatStateOf(progress)
+    }
+
+    LaunchedEffect(key1 = progressTransition) {
+        while (progressTransition != progress) {
+            if (progressTransition > progress)
+                progress += 0.01F
+            else if (progressTransition < progress)
+                progress -= 0.01F
+
+            delay(1)
+        }
+    }
+
+    Slider(
+        value = progress,
+        onValueChange = { newProgress ->
+            Log.d("CARLOS", "NEW VALUE -> $newProgress")
+            progressTransition = newProgress
+        },
+        onValueChangeFinished = {
+        },
+        steps = 5,
+        valueRange = -3f..3f,
+        colors = SliderDefaults.colors(
+            activeTrackColor = colorResource(id = R.color.deep_purple_400),
+            inactiveTrackColor = colorResource(id = R.color.deep_purple_400),
+            inactiveTickColor = colorResource(id = R.color.orangeTextColor),
+            activeTickColor = colorResource(id = R.color.orangeTextColor)
+        ),
+        thumb = { Icon(imageVector = Icons.Default.ThumbUp, contentDescription = "icon") }
+    )
+}
